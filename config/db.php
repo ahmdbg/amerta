8a3ad3bcbe7324ef555301c9b4eaf794acb7ccf2
Ahmad Hasan <aaahmaaad.hsn@gmail.com>
2025-05-04 19:12:13 +0700

    Update database connection credentials in db.php

--- config/db.php
+++ config/db.php
@@ -1,8 +1,8 @@
 <?php
 $hostname = 'localhost';
-$username = 'root';
-$password = '';
-$database = 'amerta_db';
+$username = 'u255726978_amerta_db';
+$password = '!Wops5Wj@5';
+$database = 'u255726978_amerta_db';
 
 $conn = mysqli_connect($hostname, $username, $password, $database);
 if (!$conn) {
